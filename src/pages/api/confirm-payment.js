7a28289b5b64de2721dcc07f1571bf2e2b3b1d4c
kiyoooo-beep <arzaliafithri@gmail.com>
2026-07-27 23:44:32 +0700

    Twice Keychain

--- src/pages/api/confirm-payment.js
+++ src/pages/api/confirm-payment.js
@@ -1,5 +1,5 @@
 // src/pages/api/confirm-payment.js
-import { sendCustomerConfirmation } from 'src/pages/api/resend';
+import { sendCustomerConfirmation } from 'src/pages/api/resend.js';
 
 export const prerender = false;
 
