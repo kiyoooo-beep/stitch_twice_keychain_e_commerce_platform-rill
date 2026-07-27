cefda0fcf28be1bc1d2cc4cb724de2ad00c9bbe2
kiyoooo-beep <arzaliafithri@gmail.com>
2026-07-28 00:32:29 +0700

    Twice Keychain FIX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: node({ mode: 'standalone' }),
 });
